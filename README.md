a98091e690f9015b1ef98d3772b00f0d97423b4f
Jason Shao <jasonshao8@gmail.com>
2015-09-04 23:32:33 +0200

    change title name and readme

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # riot-api-challenge
-Submission for the second Riot Games API challenge
+This is the continuation of work done for the submission of the second Riot Games API challenge.
 
 Hosted at http://ap.hpp3.me/
--- apitems/routes/index.js
+++ apitems/routes/index.js
@@ -5,17 +5,17 @@
 var data = {}
 
 exports.index = function(req, res){
-  res.render('index', { title: 'APItems' })
+  res.render('index', { title: 'AP Items' })
 };
 exports.items = function(req, res){
-  res.render('items', { title: 'APItems' })
+  res.render('items', { title: 'AP Items' })
 };
 exports.champ_chart = function(req, res){
-  res.render('champ_chart', { title: 'APItems' })
+  res.render('champ_chart', { title: 'AP Items' })
 };
 exports.champ_page = function(req, res){
   res.render('champ_page', {id: req.param.id, ap: req.query.ap})
 };
 exports.about = function(req, res){
-  res.render('about', { title: 'APItems' })
+  res.render('about', { title: 'AP Items' })
 };
